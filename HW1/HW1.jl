858f99cf3ed388b14ca390b8232f00f7b535cce4
Minh-Phuong Tran <minh-phuong.tran@student.uclouvain.be>
2022-04-17 00:21:03 +0200

    Add time

--- HW1/HW1.jl
+++ HW1/HW1.jl
@@ -17,7 +17,7 @@ function getVariables(n,m)
 
     s = s* sum_d
     d = d* sum_s
-    M = [min(s[div(k, m, RoundUp)],d[if mod(k,m)!= 0 mod(k,m) else m end]) for k in 1:n*m]
+    M = [min(s[div(k, m, RoundUp)],d[mod(k-1,m)+1]) for k in 1:n*m]
     # println(s)
     # println(d)
     # println(M)
@@ -25,12 +25,12 @@ function getVariables(n,m)
 end
 
 function Q4(n,m,c,f,s,d,M,display)
-    println("=============== Gurbi Solver ========================")
+    println("=============== Gurobi Solver ========================")
     model = Model(optimizer_with_attributes(Gurobi.Optimizer, "OutputFlag" => 0))
     @variable(model, x[1:(n*m)] >= 0)
     @variable(model, y[1:n*m], Bin)
 
-    @objective(model, Min, sum(c .* x) + sum(f .*y))
+    @objective(model, Min, sum(c.*x)+ sum(f.*y))
 
 
     @constraint(model, c1, [sum([x[(i-1)*m+j] for j in 1:m ]) for i in 1:n] .== s)
@@ -45,11 +45,11 @@ function Q4(n,m,c,f,s,d,M,display)
         @show primal_status(model)
         @show dual_status(model)
         @show objective_value(model)
-        @show value.(x) #  @show value.(x[1:n*m])
-        @show value.(y) #  @show value.(y[1:n*m])
+        #@show value.(x) #  @show value.(x[1:n*m])
+        #@show value.(y) #  @show value.(y[1:n*m])
     end
     
-    return 
+    return objective_value(model)
 end
 function Q5(n,m,c,f,s,d,M,tol,display)
     #Benders Decomposition
@@ -73,7 +73,8 @@ function Q5(n,m,c,f,s,d,M,tol,display)
     Optimality_Cut_Counter = 0
     Feasability_Cut_Counter= 0
 
-    while(true)
+    keepgoing = true
+    while(keepgoing)
 
         optimize!(master)
         t_status = termination_status(master)# == MOI.Success (if the call was succesful or stopped during)
@@ -101,20 +102,22 @@ function Q5(n,m,c,f,s,d,M,tol,display)
             break
         end
 
-        if mod(k,5)==0
+        
+        if mod(k,10)==0
 
             println("===============iter k = ",k,"========================")
             println("Lb = ",Lb,"    Ub = ",Ub)
         end
+        
 
         #with Gurobi
         subProblem = Model(optimizer_with_attributes( () -> Gurobi.Optimizer(gurobi_env),"OutputFlag"=>0))
-        set_optimizer_attribute(subProblem,"presolve",0)
+        set_optimizer_attribute(subProblem,"presolve",0);
         @variable(subProblem, v[1:n])
         @variable(subProblem, u[1:m])
         @variable(subProblem, w[1:(n*m)]>=0)
         @objective(subProblem, Max, sum(v .*s) + sum(u .*d) - sum(M .* w .* y0))
-        @constraint(subProblem,c1, [ (v[div(k,m,RoundUp)] + u[if mod(k,m)!= 0 mod(k,m) else m end] - w[k]) for k in 1:n*m] .<= c)
+        @constraint(subProblem,c1, [ (v[div(k,m,RoundUp)] + u[mod(k-1,m)+1] - w[k]) for k in 1:n*m] .<= c)
 
         optimize!(subProblem)  
 
@@ -124,18 +127,42 @@ function Q5(n,m,c,f,s,d,M,tol,display)
         # println(t_status_sub)
         # println(p_status_sub)
 
+
+        # we are done
+        if p_status_sub == MOI.FEASIBLE_POINT &&  x0 >= objective_value(subProblem) - 1e-10 
+            println("finish in ",k," iterations")
+            #println("y0=",y0)
+            #println("x0=",x0)
+            println("Feasability_Cut_Counter=",Feasability_Cut_Counter)
+            println("Optimality_Cut_Counter =",Optimality_Cut_Counter)
+
+            println("Lb=",Lb," f*= ",objective_value(master)," Ub= ",Ub)
+            keepgoing = false 
+ 
+        elseif (abs(Lb-Ub)<tol || k>10000)
+            println("other breaking condition")
+            println("finish in ",k," iterations")
+            #println("y0=",y0)
+            #println("x0=",x0)
+            println("Optimality_Cut_Counter =",Optimality_Cut_Counter)
+            println("Feasability_Cut_Counter=",Feasability_Cut_Counter)
+            println("Lb=",Lb," f*= ",objective_value(master)," Ub= ",Ub)
+
+            keepgoing = false
+        end
+
         # if unbounded, add the feasibility cut (There is an  extreme ray, adding the corresponding constraint)
-        if t_status_sub == MOI.DUAL_INFEASIBLE && p_status_sub == MOI.INFEASIBILITY_CERTIFICATE
+        if  keepgoing && (t_status_sub == MOI.DUAL_INFEASIBLE && p_status_sub == MOI.INFEASIBILITY_CERTIFICATE)
             # println("-->extreme rays")
             ve = value.(v)
             ue = value.(u)
             we = value.(w)
             @constraint(master, sum(ve .*s) + sum(ue .*d) - sum(M .* we .* y ) <= 0) #TODO
             Feasability_Cut_Counter += 1
-        end
+        
         #if bounded and add optimality cut (add a vertex)
 
-        if p_status_sub == MOI.FEASIBLE_POINT && x0 < objective_value(subProblem) 
+        elseif keepgoing && (p_status_sub == MOI.FEASIBLE_POINT && x0 < objective_value(subProblem)) 
             #  println("-->vertice")
             #  println(x0," <" ,  objective_value(subProblem) )
             vv = value.(v)
@@ -143,37 +170,40 @@ function Q5(n,m,c,f,s,d,M,tol,display)
             wv = value.(w)
             @constraint(master, sum(vv .*s ) + sum(uv .*d ) - sum(M .* wv .* y)<= x )#TODO
             Ub = sum(f.*y0)+ objective_value(subProblem)
-            Optimality_Cut_Counter +=1
-        end 
-        # we are done
-        if p_status_sub == MOI.FEASIBLE_POINT &&  x0 >= objective_value(subProblem) 
-            println("finish in ",k," iterations")
-            println("y0=",y0)
-            println("x0=",x0)
-            println("Lb=",Lb," f*= ",objective_value(master)," Ub= ",Ub)
-            println("Optimality_Cut_Counter =",Optimality_Cut_Counter)
-            println("Feasability_Cut_Counter=",Feasability_Cut_Counter)
-            break
-        end  
-
-        if abs(Lb-Ub)<1e-2 || k>10000
-            println("other breaking condition")
-            println("finish in ",k," iterations")
-            println("y0=",y0)
-            println("x0=",x0)
-            println("Lb=",Lb," f*= ",objective_value(master)," Ub= ",Ub)
-            println("Optimality_Cut_Counter =",Optimality_Cut_Counter)
-            println("Feasability_Cut_Counter=",Feasability_Cut_Counter)
-            break
+            Optimality_Cut_Counter +=1 
         end
+
 
         k += 1
         
     end
 
-    return
+    return objective_value(master)
 end
-n,m,c,f,s,d,M = getVariables(5,10)
 
-#Q4(n,m,c,f,s,d,M,true)
-Q5(n,m,c,f,s,d,M,1e-2,true)
+
+function compareTime(n,m)
+
+    n,m,c,f,s,d,M = getVariables(n,m)
+
+    time1 = @elapsed obj1 = Q4(n,m,c,f,s,d,M,true);
+
+    n,m,c,f,s,d,M = getVariables(n,m)
+
+    time3 = @elapsed obj1 = Q4(n,m,c,f,s,d,M,true);
+
+    time4 = @elapsed obj2 = Q5(n,m,c,f,s,d,M,1e-2,true);
+
+
+    return time1, time3, time4, abs(obj1 -obj2)
+end
+
+
+time1, time3, time4, diff = compareTime(15,15);
+println("time1 = ",time1)
+println("time3 = ",time3)
+println("time4 = ",time4)
+println("diff = ",diff)
+
+
+
---
+++ HW1/test.jl
@@ -0,0 +1,36 @@
+#ligne 20
+import Pkg; Pkg.add("Gurobi")
+using LinearAlgebra
+function f(k,m)
+
+	if mod(k,m)!= 0 y = mod(k,m) else y = m end
+	y
+end
+
+function f2(k,m)
+	y = mod(k-1,m)+1
+	# y = ((k-1)%m) + 1
+end
+
+
+#Ligne 33
+# dot(c,x)
+
+
+
+a = [1 1 1]
+b = [3 4 5]
+println(sum(a.*b))
+println(dot(a,b))
+
+# in benders, optimal x?
+# why k<3 in while loop ?
+# tol instead of 10e-2
+
+#=
+
+For the larger case initially in scope, you can also  on the convergence status you obtain, to document how slow is the Benders decomposition for your problem, the final gap you have, or the quality of the solution you obtain when stopping the process before convergence (comparing objective values with the objective for the optimal one).
+
+Last but not least, as you know, floating-point arithmetic issues require you to be cautious when you compare numbers to trigger actions such as adding Benders cuts, etc. Consider for example taking into account very small tolerances "epsilon" (e.g. ~1e-6 or 1e-7, etc, depending on the context) when checking conditions such as "a >= b".
+
+=#
